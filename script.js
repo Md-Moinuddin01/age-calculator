674736c84dd5941865c18b646de5c70d9094a90a
Md Moinuddin <191840000+Md-Moinuddin01@users.noreply.github.com>
2026-07-24 04:14:31 +0530

    Implement age calculation from birthdate input
    Added age calculation based on user birthdate input and implemented validation for future dates.

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 const birthdateInput = document.getElementById('birthdate');
 const calculateBtn = document.getElementById('calculateBtn');
 const resultDiv = document.getElementById('result');
+
+let liveTimer;
+
+calculateBtn.addEventListener('click', () => {
+    const birthdate = birthdateInput.value;
+
+    // Stop the previous timer
+    clearInterval(liveTimer);
+
+    if (birthdate) {
+        // Adding time prevents date/timezone calculation issues
+        const birthDate = new Date(`${birthdate}T00:00:00`);
+        const today = new Date();
+
+        // Prevent future birthdates
+        if (birthDate > today) {
+            resultDiv.textContent = 'Birthdate cannot be in the future Yaar 🙋🏻‍♂️';
+            return;
+        }
+
+        let age = today.getFullYear() - birthDate.getFullYear();
+        const monthDifference = today.getMonth() - birthDate.getMonth();
+
+        if (
+            monthDifference < 0 ||
+            (monthDifference === 0 && today.getDate() < birthDate.getDate())
+        ) {
+            age--;
+        }
+
+      
